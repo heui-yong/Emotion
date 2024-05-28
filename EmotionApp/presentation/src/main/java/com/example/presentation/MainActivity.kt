c20aa30525505cb400af0a5031f63a3273cd7a26
heui-yong <gusrnr9878@gmail.com>
2024-05-29 08:53:06 +0900

    [Emotion]:[aos-ui] 간단한 UI구성   - 텍스트 입력 영역 추가   - 분석을 위한 버튼 추가   - 결과값을 보여줄 텍스트 영역 추가

--- EmotionApp/presentation/src/main/java/com/example/presentation/MainActivity.kt
+++ EmotionApp/presentation/src/main/java/com/example/presentation/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.presentation
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.presentation.databinding.ActivityMainBinding
 import com.example.presentation.viewmodel.EmotionViewModel
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+
     private val binding: ActivityMainBinding by lazy {
         DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
@@ -18,6 +30,41 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        viewModel.test()
+
+        initView()
+        initObserve()
+    }
+
+    private fun initView() {
+        binding.viewCallback = viewCallback
+    }
+
+    private fun initObserve() {
+        repeatOn(Lifecycle.State.STARTED) {
+            viewModel.emotionData.collect{
+                Log.e(TAG, "data = $it")
+                binding.emotionData = it
+            }
+        }
+    }
+
+    interface ViewCallback {
+        fun onClick(view: View)
+    }
+
+    private val viewCallback = object : ViewCallback {
+        override fun onClick(view: View) {
+            when(view.id) {
+                R.id.btn_emotion_recognition -> {
+                    viewModel.getEmotionData(binding.edTxInput.text.toString())
+                }
+            }
+        }
+    }
+}
+
+fun LifecycleOwner.repeatOn(state: Lifecycle.State, block: suspend CoroutineScope.() -> Unit) {
+    lifecycleScope.launch {
+        repeatOnLifecycle(state, block)
     }
 }
